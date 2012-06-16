e3c95d8285dfc3d844fe863b1f6ca9fdbc877e07
Jonas Haag <jonas@lophus.org>
2012-06-16 13:05:10 +0200

    Added Flatshare and UserProfile models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Flatshare(models.Model):
+    # TODO geo coordinates
+    address = models.TextField()
+    max_guests = models.PositiveIntegerField(validators=[MinValueValidator(2)])
+
+
+class UserProfile(models.Model):
+    year_of_birth = models.PositiveIntegerField()
